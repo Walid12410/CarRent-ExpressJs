d98f499c7ed7542c0fac5bb747920fa1a23e7941
walid jallad <78869109+Walid12410@users.noreply.github.com>
2024-09-30 14:29:29 +0300

    carRentAggregation

--- aggregation/carRentAggregation.js
+++ aggregation/carRentAggregation.js
@@ -77,48 +77,6 @@ const carRentAdminAggregation = [
 ];
 
 
-const carRentCategoryAggregation = [
-    {
-        $match: {
-            carStatus: "available"
-        }
-    },
-    {
-        $lookup:{
-            from : "reviews",
-            localField :"_id",
-            foreignField :"carId",
-            as :"reviews"
-        }
-    },
-    {
-        $lookup:{
-            from :"carimages",
-            localField : "_id",
-            foreignField : "carRentID",
-            as :"CarImage"
-        }
-    },
-    {
-        $sort: {
-            createdAt: -1 
-        }
-    },
-    {
-        $addFields:{
-            reviewCount: { $size: { $ifNull: ['$reviews', []] } }, // Provide default empty array
-            averageRating: { $avg: { $ifNull: ['$reviews.rate', []] } }, // Provide default empty array
-        }
-    },
-    {
-        $project: {
-            reviews: 0,
-        },
-    }
-
-];
-
-
 module.exports = {carRentAggregation,
     carRentAdminAggregation
 };
